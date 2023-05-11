1be843b7a39d870d888e605007fa2e35dd9e3d88
unknown <kristen.belkadi@epsi.fr>
2023-05-11 11:22:01 +0200

    init

---
+++ src/Lib.hs
@@ -0,0 +1,24 @@
+module Lib where
+
+import Data.Set as Set
+import Graphics.WorldTurtle
+
+data LSysteme = LSysteme
+  { variables :: Set Char,
+    constantes :: Set Char,
+    axiome :: String,
+    regles :: Char -> String
+  }
+
+generer :: LSysteme -> Int -> String
+generer ls 0 = axiome ls
+generer ls n = Prelude.foldl (\acc elm -> acc ++ regles ls elm) "" (generer ls (n - 1))
+
+executer ls n = runTurtle ( setSpeed 0 >> (Prelude.foldl (\acc e -> acc >> (case e of 'F' -> forward 5; '+' -> left 90; '-' -> right 90; _ -> forward 0)) (return ()) (generer ls n)))
+
+koch :: LSysteme
+koch = LSysteme {variables = Set.singleton 'F', constantes = Set.fromList "+-", axiome = "F", regles = \c -> case c of 'F' -> "F+F-F-F+F"; _ -> [c]}
+
+someFunc :: IO ()
+someFunc = do
+  executer koch 3
